7f59fe47e15604e64e4f0fa2633d36a2ea91f720
Duarte Alves <duarte.alves@tecnico.ulisboa.pt>
2018-05-30 23:21:01 +0100

    Plugin testing

--- index.js
+++ index.js
@@ -61,24 +61,24 @@ app.listen(app.get('port'), function() {
 	console.log("running: port")
 })
 
-<script>
-window.fbAsyncInit = function() {
-    FB.init({
-      appId            : '1909517959098834',
-      autoLogAppEvents : true,
-      xfbml            : true,
-      version          : 'v3.0'
-    });
-  };
-
-(function(d, s, id) {
-	  var js, fjs = d.getElementsByTagName(s)[0];
-	  if (d.getElementById(id)) return;
-	  js = d.createElement(s); js.id = id;
-	  js.src = 'https://connect.facebook.net/pt_PT/sdk/xfbml.customerchat.js#xfbml=1&version=v2.12&autoLogAppEvents=1';
-	  fjs.parentNode.insertBefore(js, fjs);
-	}(document, 'script', 'facebook-jssdk'));
-
-	<!-- Your customer chat code -->
-	jQuery('body').append('<div class="fb-customerchat" page_id="226748641432465" theme_color="#fa3c4c" logged_in_greeting="Olá! Como posso ajudar-lhe?" logged_out_greeting="Olá! Como posso ajudar-lhe?"> </div>');
-</script>
+//<script>
+//window.fbAsyncInit = function() {
+//    FB.init({
+//      appId            : '1909517959098834',
+//      autoLogAppEvents : true,
+//      xfbml            : true,
+//      version          : 'v3.0'
+//    });
+//  };
+//
+//(function(d, s, id) {
+//	  var js, fjs = d.getElementsByTagName(s)[0];
+//	  if (d.getElementById(id)) return;
+//	  js = d.createElement(s); js.id = id;
+//	  js.src = 'https://connect.facebook.net/pt_PT/sdk/xfbml.customerchat.js#xfbml=1&version=v2.12&autoLogAppEvents=1';
+//	  fjs.parentNode.insertBefore(js, fjs);
+//	}(document, 'script', 'facebook-jssdk'));
+//
+//	<!-- Your customer chat code -->
+//	jQuery('body').append('<div class="fb-customerchat" page_id="226748641432465" theme_color="#fa3c4c" logged_in_greeting="Olá! Como posso ajudar-lhe?" logged_out_greeting="Olá! Como posso ajudar-lhe?"> </div>');
+//</script>
