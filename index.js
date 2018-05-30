4ac4c295fbcdc68da474a3326da2e611b2cb36b1
Duarte Alves <duarte.alves@tecnico.ulisboa.pt>
2018-05-30 22:47:51 +0100

    error fix

--- index.js
+++ index.js
@@ -29,3 +29,33 @@ app.get('/webhook/', function(req, res) {
 app.listen(app.get('port'), function() {
 	console.log("running: port")
 })
+
+<script>
+window.fbAsyncInit = function() {
+    FB.init({
+      appId            : '1909517959098834',
+      autoLogAppEvents : true,
+      xfbml            : true,
+      version          : 'v3.0'
+    });
+  };
+<body>
+	<!-- Load Facebook SDK for JavaScript -->
+	<div id="fb-root"></div>
+	<script>(function(d, s, id) {
+	  var js, fjs = d.getElementsByTagName(s)[0];
+	  if (d.getElementById(id)) return;
+	  js = d.createElement(s); js.id = id;
+	  js.src = 'https://connect.facebook.net/pt_PT/sdk/xfbml.customerchat.js#xfbml=1&version=v2.12&autoLogAppEvents=1';
+	  fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));</script>
+
+	<!-- Your customer chat code -->
+	<div class="fb-customerchat"
+	  attribution="setup_tool"
+	  page_id="226748641432465"
+	  theme_color="#fa3c4c"
+	  logged_in_greeting="Olá! Como posso ajudar-lhe?"
+	  logged_out_greeting="Olá! Como posso ajudar-lhe?">
+	</div></script>
+</body>
